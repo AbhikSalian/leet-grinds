0ace61b6bcb7da85c5371652753409e2e5db449a
Abhik L Salian <109756532+AbhikSalian@users.noreply.github.com>
2024-09-10 18:16:42 +0530

    Time: 225 ms (7.92%), Space: 17.3 MB (59.34%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         vow=0
-        maxvow=0
         n=len(s)
         for i in range(k):
             if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u':
